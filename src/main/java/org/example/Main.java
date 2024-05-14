2f14304dec596e363e1983fb2b09e0a3b1993a50
MykhailoHrybchenko <m.grybchenko@gmail.com>
2024-05-14 16:37:27 +0300

    -added complexity overview for each class -added link to uml diagram in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,3 +7,7 @@ public class Main {
         System.out.println(DynamicFibonacci.calcDynamicFibonacci(5L));
     }
 }
+/*
+link to uml:
+https://lucid.app/lucidchart/f3de8e2f-d341-4324-95b4-e6c61cb659dd/edit?viewport_loc=-1455%2C-750%2C2452%2C1120%2C0_0&invitationId=inv_45e6291a-bcc7-4fe9-aaf8-74d2b6e8b921
+ */
